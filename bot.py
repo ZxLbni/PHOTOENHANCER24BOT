8023e44624c74c4ed6f890ee17198c757f85071b
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-06-09 16:53:14 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -890,10 +890,10 @@ async def get_stats(client, message):
     await mr.edit( text=f"🔍 TOTAL USER'S = `{total_users}`")
 
 @app.on_message(filters.command("broadcast") & filters.user(ADMIN) & filters.reply)
-async def broadcast_handler(bot: Client, m: Message):
+async def broadcast_handler(bot: client, m: message):
     all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    sts_msg = await m.reply_text("broadcast started !") 
+    broadcast_msg = message.reply_to_message
+    sts_msg = await message.reply_text("broadcast started !") 
     done = 0
     failed = 0
     success = 0
@@ -917,27 +917,27 @@ async def broadcast_handler(bot: Client, m: Message):
 @app.on_message(filters.command(["id", "info"]))
 async def media_info(client, message): 
     message = m
-    ff = m.from_user
-    md = m.reply_to_message
+    ff = message.from_user
+    md = message.reply_to_message
     if md:
        try:
           if md.photo:
-              await m.reply_text(text=f"**your photo id is **\n\n`{md.photo.file_id}`") 
+              await message.reply_text(text=f"**your photo id is **\n\n`{md.photo.file_id}`") 
           if md.sticker:
-              await m.reply_text(text=f"**your sticker id is **\n\n`{md.sticker.file_id}`")
+              await message.reply_text(text=f"**your sticker id is **\n\n`{md.sticker.file_id}`")
           if md.video:
-              await m.reply_text(text=f"**your video id is **\n\n`{md.video.file_id}`")
+              await message.reply_text(text=f"**your video id is **\n\n`{md.video.file_id}`")
           if md.document:
-              await m.reply_text(text=f"**your document id is **\n\n`{md.document.file_id}`")
+              await message.reply_text(text=f"**your document id is **\n\n`{md.document.file_id}`")
           if md.audio:
-              await m.reply_text(text=f"**your audio id is **\n\n`{md.audio.file_id}`")
+              await message.reply_text(text=f"**your audio id is **\n\n`{md.audio.file_id}`")
           if md.text:
-              await m.reply_text("**hey man please reply with ( photo, video, sticker, documents, etc...) Only media **")  
+              await message.reply_text("**hey man please reply with ( photo, video, sticker, documents, etc...) Only media **")  
           else:
-              await m.reply_text("[404] Error..🤖")                                                                                      
+              await message.reply_text("[404] Error..🤖")                                                                                      
        except Exception as e:
           print(e)
-          await m.reply_text(f"[404] Error {e}")
+          await message.reply_text(f"[404] Error {e}")
                                         
     if not md:
         buttons = [[
