afe40e35a45a565a56231d58bf8d5d33562ff56b
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-06-09 16:32:51 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -19,7 +19,11 @@ from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 import datetime
 from helper.database import db
+import pyrogram, random
+from pyrogram import enums
 
+
+INFO = """• Send /id or info to get your telegram id & info 💝"""
 
 # Store the photos temporarily in a dictionary
 photo_dict = {}
@@ -909,18 +913,59 @@ async def broadcast_handler(bot: Client, m: Message):
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts_msg.edit(f"Broadcast Completed:\nCompleted in `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
  
-         
-
-
-
-
-
-
-
-
-
-
-
+#info text                                              
+@app.on_message(filters.command(["id", "info"]))
+async def media_info(client, message): 
+    message = m
+    ff = m.from_user
+    md = m.reply_to_message
+    if md:
+       try:
+          if md.photo:
+              await m.reply_text(text=f"**your photo id is **\n\n`{md.photo.file_id}`") 
+          if md.sticker:
+              await m.reply_text(text=f"**your sticker id is **\n\n`{md.sticker.file_id}`")
+          if md.video:
+              await m.reply_text(text=f"**your video id is **\n\n`{md.video.file_id}`")
+          if md.document:
+              await m.reply_text(text=f"**your document id is **\n\n`{md.document.file_id}`")
+          if md.audio:
+              await m.reply_text(text=f"**your audio id is **\n\n`{md.audio.file_id}`")
+          if md.text:
+              await m.reply_text("**hey man please reply with ( photo, video, sticker, documents, etc...) Only media **")  
+          else:
+              await m.reply_text("[404] Error..🤖")                                                                                      
+       except Exception as e:
+          print(e)
+          await m.reply_text(f"[404] Error {e}")
+                                        
+    if not md:
+        buttons = [[
+            InlineKeyboardButton("✨️ Support", url="https://t.me/Sunrises24botsupport"),
+            InlineKeyboardButton("📢 Updates", url="https://t.me/Sunrises24botupdates")
+        ]]       
+        sh = await m.reply("please wait....")
+        if ff.photo:
+           user_dp = await bot.download_media(message=ff.photo.big_file_id)
+           await m.reply_photo(
+               photo=user_dp,
+               caption=txt.INFO_TXT.format(id=ff.id, dc=ff.dc_id, n=ff.first_name, u=ff.username),
+               reply_markup=InlineKeyboardMarkup(buttons),
+               quote=True,
+               parse_mode=enums.ParseMode.HTML,
+               disable_notification=True
+           )          
+           os.remove(user_dp)
+           await sh.delete()
+        else:  
+           await m.reply_text(
+               text=txt.INFO_TXT.format(id=ff.id, dc=ff.dc_id, n=ff.first_name, u=ff.username),
+               reply_markup=InlineKeyboardMarkup(buttons),
+               quote=True,
+               parse_mode=enums.ParseMode.HTML,
+               disable_notification=True
+            )
+                     
 
 # Run the bot
 app.run()
